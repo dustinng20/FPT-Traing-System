d5c4188e079ef466304c9e71f7f72cb1b42ab41d
Dustin Nguyen <huongkhe123@gmail.com>
2021-06-10 20:58:41 +0700

    AddCategoryModel

---
+++ FPT Traing System/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FPT_Traing_System.Models
+{
+	public class Category
+	{
+		[Required]
+		[Key]
+		public string Id { get; set; }
+
+		[Required]
+		[StringLength(225)]
+		[DisplayName("Category Name")]
+		[Index("Name_Index", IsUnique = true)]
+		public string Name { get; set; }
+		
+		[Required]
+		public string Description { get; set; }
+	}
+}
---
+++ FPT Traing System/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FPT_Traing_System.Models
+{
+	public class Course
+	{
+	
+		[Required]
+		public int CourseId { get; set; }
+	
+		
+		[Required]
+		public int Id { get; set; }
+		
+		
+		[Required]
+		public string Name { get; set; }
+	
+		
+		[Required]
+
+		public string Description { get; set; }
+	}
+}
